2c5482e8dfa7f2a5d821d827b95b34cf7e4bef8e
Jen O <jenniogbodo@gmail.com>
2026-08-04 20:43:30 -0400

    Initial Commit

---
+++ Keep Me Up! Watch App/Keep_Me_Up_App.swift
@@ -0,0 +1,17 @@
+//
+//  Keep_Me_Up_App.swift
+//  Keep Me Up! Watch App
+//
+//  Created by Jennifer ogbodo on 8/4/26.
+//
+
+import SwiftUI
+
+@main
+struct Keep_Me_Up__Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
